79f9da5b7995da7a9d74c5343ea4afc6716c5477
Dmitry Parshukov <dimer3799@yandex.ru>
2021-03-08 21:01:17 +0600

    udate logic game

--- Fabrica/main.py
+++ Fabrica/main.py
@@ -18,16 +18,39 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.label_9.setText('Количество: ' + str(product))
         self.label_6.setText('Стоимость: ' + str(price_product))
         self.pushButton.clicked.connect(self.by_material)
+        self.pushButton_2.clicked.connect(self.farm_product)
+        self.pushButton_3.clicked.connect(self.sell_product)
 
     def by_material(self):
-        global money, price_material, price_product, sklad, material, product
+        global money, price_material, sklad, material
         if (money >= price_material) and (sklad <= 297):
-            material += 3
-            sklad += 3
+            material += 1
+            sklad += 1
             money -= price_material
             self.label.setText('Деньги: ' + str(money))
             self.label_8.setText('Количество: ' + str(material))
             self.label_5.setText('Стоимость: ' + str(price_material))
+            self.progressBar.setValue(sklad)
+
+    def farm_product(self):
+        global money, sklad, material, product
+        if material >= 5:
+            material -= 5
+            sklad -= 2
+            product += 3
+            self.label_8.setText('Количество: ' + str(material))
+            self.label_9.setText('Количество: ' + str(product))
+            self.progressBar.setValue(sklad)
+
+    def sell_product(self):
+        global money,  price_product, sklad, product
+        if product >= 1:
+            money += price_product
+            sklad -= 1
+            product -= 1
+            self.label_8.setText('Количество: ' + str(material))
+            self.label_9.setText('Количество: ' + str(product))
+            self.label.setText('Деньги: ' + str(money))
             self.progressBar.setValue(sklad)
 
 
